757a179e7a8e701bdfcc29e3bfaffb078ed3dc7d
Einar Arro <einar.arro@eesti.ee>
2014-02-26 09:00:58 +0200

    Korrutamise lisamine ja raskusaste refactor

--- PeastArvutamine.py
+++ PeastArvutamine.py
@@ -1,7 +1,6 @@
 __author__ = 'einar'
 
 import random
-import time
 
 manguPikkus = 30
 punktideCounter = 0
@@ -10,25 +9,6 @@ print("{:*^75}".format(" PEAST ARVUTAMISE MÄNG "))
 print("** Iga õige vastus annab ühe punkti. Iga vale vastus võtab kaks punkti maha")
 print("** Mäng lõppeb, kui oled kokku kogunud {0} punkti".format(manguPikkus))
 print("{:*^75}".format("*"))
-
-
-def arvutaRaskusaste(param):
-    try:
-        raskusaste = 0
-        if param == 1:
-            raskusaste = 10
-        elif param == 2:
-            raskusaste = 20
-        elif param == 3:
-            raskusaste = 30
-        elif param == 4:
-            raskusaste = 40
-        else:
-            v = input("Sinu sisestatud raskusaste ei ole lubatud. Väljumiseks vajuta Enter ja taaskäivita mäng")
-            raise SystemExit
-        return raskusaste
-    except (AttributeError, TypeError):
-        raise AssertionError("Raskusaste peab olema int tüüpi")
 
 
 def oigeVoiVale(param1, param2):
@@ -42,14 +22,13 @@ def oigeVoiVale(param1, param2):
         else:
             punktideCounter += 1
             print()
-            #print(punktideCounter)
     except (AttributeError, TypeError):
         raise AssertionError("Õige ja vale vastus ei ole õiget tüüpi")
 
 
 def liitmine():
-    liidetav = random.randint(1, raskusaste)
-    liitja = random.randint(1, raskusaste)
+    liidetav = random.randint(1, suurim_number)
+    liitja = random.randint(1, suurim_number)
     try:
         vastus = int(input("{0} + {1} = ".format(liidetav, liitja)))
     except (AttributeError, TypeError):
@@ -59,13 +38,23 @@ def liitmine():
 
 
 def lahutamine():
-    lahutatav = random.randint(2, raskusaste)
+    lahutatav = random.randint(2, suurim_number)
     lahutaja = random.randint(1, lahutatav)
     try:
         vastus = int(input("{0} - {1} = ".format(lahutatav, lahutaja)))
     except (AttributeError, TypeError):
         raise AssertionError("Sa ei sisestanud täisarvu")
     oigeVastus = lahutatav - lahutaja
+    oigeVoiVale(oigeVastus, vastus)
+
+def korrutamine():
+    korrutatav = random.randint(1, 10)
+    korrutaja = random.randint(1, suurim_number)
+    try:
+        vastus = int(input("{0} * {1} = ".format(korrutaja, korrutatav)))
+    except (AttributeError, TypeError):
+        raise AssertionError("Sa ei sisestanud täisarvu")
+    oigeVastus = korrutatav * korrutaja
     oigeVoiVale(oigeVastus, vastus)
 
 
@@ -75,45 +64,47 @@ def tehteValik(tehe):
             liitmine()
         elif tehe == 2:
             lahutamine()
+        elif tehe == 4:
+            korrutamine()
         else:
             v = input("Sinu sisestatud tehtevalik ei ole lubatud. Väljumiseks vajuta Enter ja taaskäivita mäng")
             raise SystemExit
     except (AttributeError, TypeError):
         raise AssertionError("Tehte valik peab olema int tüüpi")
 
-
-def prindiAeg(start, end):
-    print("Aeg: {0:.0f} sekundit".format(end - start))
-
-
+#### tehte valik ###
 print()
-print("Vali raskusaste:     1: kuni 10     2: kuni 20     3: kuni 30     4: kuni 40")
-
-try:
-    raskusaste_in = int(input())
-except (ValueError):
-    v = input("Sinu sisestatud raskusaste ei ole lubatud. Väljumiseks vajuta Enter ja taaskäivita mäng")
-    raise SystemExit
-
-raskusaste = arvutaRaskusaste(raskusaste_in)
-
-print("Vali tehe:     1: liitmine     2: lahutamine     3: liitmine ja lahutamine")
-
+print("Vali tehe:     1: liitmine     2: lahutamine     3: liitmine ja lahutamine     4: korrutamine")
 try:
     tehe = int(input())
 except (ValueError):
     v = input("Sinu sisestatud tehtevalik ei ole lubatud. Väljumiseks vajuta Enter ja taaskäivita mäng")
     raise SystemExit
 
-start = time.time()
+if tehe == 1:
+    teheSonadega = "liitmist"
+elif tehe == 2:
+    teheSonadega = "lahutamist"
+elif tehe == 3:
+    teheSonadega = "liitmist ja lahutamist"
+elif tehe == 4:
+    teheSonadega = "korrutamist"
 
+#### raskusastme valik ###
+print()
+print("Sisesta suurim number, mille piires {0} harjutada: ".format(teheSonadega))
+try:
+    suurim_number = int(input())
+except (ValueError):
+    v = input("Sinu sisestatud raskusaste ei ole lubatud. Väljumiseks vajuta Enter ja taaskäivita mäng")
+    raise SystemExit
+
+#### mängu käivitus ###
 while (punktideCounter < manguPikkus):
     if tehe == 3:
         tehteValik(random.randint(1, 2))
     else:
         tehteValik(tehe)
 
-end = time.time()
-diff = end - start
-print("Mängu läbimiseks kulus {0:.0f} minutit ja {1:.0f} sekundit".format((diff // 60), (diff % 60)))
-a = input("Väljumiseks vajuta Enter")
+
+v = input("Väljumiseks vajuta Enter")
